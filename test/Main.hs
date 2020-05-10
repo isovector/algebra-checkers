4e0ffc187d1c5df8cc34afd1877378fc59fb3065
Sandy Maguire <sandy@sandymaguire.me>
2020-05-10 10:57:28 -0700

    Test arbitrary generation

--- test/Main.hs
+++ test/Main.hs
@@ -1,14 +1,23 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+
 module Main where
 
 import qualified AppTest as AT
 import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
   describe "App" $ do
-    it "should generate models" $ do
-      AT.model_foo 5 `shouldBe` 6
+    it "should generate models" $
+      property $ \n ->
+        AT.model_foo n `shouldBe` (n + 1)
 
-    it "should generate empty types" $ do
-      show AT.EmptyType `shouldBe` "EmptyType"
+    describe "Empty types" $ do
+      it "should be generated" $ do
+        AT.EmptyType `shouldBe` AT.EmptyType
+
+      it "should have trivial arbitrary instances" $
+        property $ \empty ->
+          empty `shouldBe` AT.EmptyType
 
